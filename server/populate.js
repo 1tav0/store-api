fb38d2497586a3604787e56e7e948efcbfa912c4
Tavo Cruz <madecruz2@gmail.com>
2024-06-29 14:31:42 -0700

    how to populate db with json file

---
+++ server/populate.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const connectDB = require('./db/connect');
+const Product = require('./models/product');
+const products = require('./products.json');
+
+const start = async (req, res) => {
+  try {
+    await connectDB(process.env.MongoURI);
+    await Product.deleteMany();
+    await Product.create(products);
+    console.log("successfully populated db");
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+start();
